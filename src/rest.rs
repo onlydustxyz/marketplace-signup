23f6b78541e1c2201eff087f1a3453337b2fb254
Olivier Fuxet <olivier.fuxet@gmail.com>
2022-06-15 09:58:20 +0200

    feat: add health-check endpoint

--- src/rest.rs
+++ src/rest.rs
@@ -121,3 +121,12 @@ pub async fn register_github_user(
 
     Status::NoContent
 }
+
+#[derive(Responder)]
+#[response(status = 200, content_type = "json")]
+pub struct RawOk(&'static str);
+
+#[get("/health")]
+pub async fn health_check() -> RawOk {
+    RawOk("{\"status\":\"ok\"}")
+}
